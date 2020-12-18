729bb955a6ceb2204972483eedbb8e84d1ebf159
haoyxc <48307028+haoyxc@users.noreply.github.com>
2020-12-18 17:03:38 -0600

    i forgot what I did tbh

--- labeler.py
+++ labeler.py
@@ -17,6 +17,27 @@ def load_and_parse_entity_labels(file_path):
 all_labels = load_and_parse_entity_labels("./18_gold_labels/test_18_gold_labels.txt")
 # print(entities_labels)
 
+label_mappings = {
+	"O": 0,
+	"PER": 1,
+	"NORP": 2,
+	"FAC": 3,
+	"ORG": 4,
+	"GPE": 5,
+	"LOC": 6,
+	"PRODUCT": 7,
+	"EVENT": 8,
+	"WORK_OF_ART": 9,
+	"LAW": 10,
+	"LANGUAGE": 11,
+	"DATE": 12,
+	"TIME": 13,
+	"PERCENT": 14,
+	"MONEY": 15,
+	"QUANTITY": 16,
+	"ORDINAL": 17,
+	"CARDINAL": 18
+}
 def write_array_to_file(file_path, tokens):
     """
     Creates and writes all tokens in input array to new file
@@ -32,11 +53,17 @@ def write_array_to_file(file_path, tokens):
         if index != 0:
             f.write("\n")
         token = tokens[index]
-        for char_index in range(len(token)):
-            if char_index != 0:
-                f.write(" ")
-            f.write(token[char_index])
+        label_code = 0
+        if (token != "O"):
+        	dash_index = token.index("-")
+        	if (dash_index < -1):
+        		continue
+        	token = token[dash_index+1:]
+        	label_code = 0
+        	if token in label_mappings:
+        		label_code = label_mappings[token]
+        f.write(str(label_code))
     f.close()
 
-path_test_18_actual = "./NER_labels/test_18/test_18_actual.txt"
+path_test_18_actual = "./NER_labels/test_18/test_18_actual_codes.txt"
 write_array_to_file(path_test_18_actual, all_labels)
